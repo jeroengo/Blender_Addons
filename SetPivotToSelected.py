531f5290d160d5c402c0d98ca4704f2bb6b81a46
Jeroen <mjeroengorissen@gmail.com>
2024-03-13 11:14:58 +0100

    Fixed some naming in pivot tools

--- SetPivotToSelected.py
+++ SetPivotToSelected.py
@@ -1,17 +1,17 @@
+import bpy
+
 bl_info = {
-    "name": "My Blender Plugin",
-    "description": "A brief description of my plugin",
+    "name": "Pivot tools",
+    "description": "Quickly set the pivot of your object to whatever you have selected in edit mode",
     "author": "Jeroen Gorissen",
     "version": (1, 0),
-    "blender": (4, 0, 0),
+    "blender": (2, 80, 0),
     "location": "View3D > Tools",
     "category": "Object"
 }
 
-import bpy
-
 # Define custom operator
-class MyOperator(bpy.types.Operator):
+class OBJECT_OT_MyOperator(bpy.types.Operator):
     bl_idname = "object.my_operator"
     bl_label = "My Operator"
 
@@ -23,12 +23,12 @@ class MyOperator(bpy.types.Operator):
         return {'FINISHED'}
 
 # Define custom panel
-class MyPanel(bpy.types.Panel):
-    bl_label = "My Panel"
-    bl_idname = "OBJECT_PT_my_panel"
+class OBJECT_PT_MyPanel(bpy.types.Panel):
+    bl_label = "Pivot tools"
+    bl_idname = "OBJECT_PT_pivot_tools"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
-    bl_category = 'My Addon'
+    bl_category = 'Pivot tools'
 
     def draw(self, context):
         layout = self.layout
@@ -36,12 +36,12 @@ class MyPanel(bpy.types.Panel):
 
 # Register and unregister functions
 def register():
-    bpy.utils.register_class(MyOperator)
-    bpy.utils.register_class(MyPanel)
+    bpy.utils.register_class(OBJECT_OT_MyOperator)
+    bpy.utils.register_class(OBJECT_PT_MyPanel)
 
 def unregister():
-    bpy.utils.unregister_class(MyOperator)
-    bpy.utils.unregister_class(MyPanel)
+    bpy.utils.unregister_class(OBJECT_OT_MyOperator)
+    bpy.utils.unregister_class(OBJECT_PT_MyPanel)
 
 # Allow running as a script
 if __name__ == "__main__":
